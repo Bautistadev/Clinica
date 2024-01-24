2d09492b279dfe8e877644e7770b0a9ddf9247a7
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-01-24 02:31:11 -0300

    ;

--- src/main/java/com/SpringBoot/Clinica/MainApplication.java
+++ src/main/java/com/SpringBoot/Clinica/MainApplication.java
@@ -36,7 +36,7 @@ public class MainApplication implements CommandLineRunner {
 		UserRepository userRepository = this.context.getBean(UserRepository.class);
 
 
-		System.out.println(userRepository.findByUsername("lala"));
+		System.out.println(userRepository.findByUsername("Hola mundo"));
 
 	}
 }
