78ef69dc2790dc9ce2d3b5fbee668eafdc27ee6b
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-01-24 23:49:47 -0300

    FEAT: User mapper class implements

--- src/main/java/com/SpringBoot/Clinica/Service/Mapper/UserMapper.java
+++ src/main/java/com/SpringBoot/Clinica/Service/Mapper/UserMapper.java
@@ -3,12 +3,14 @@ package com.SpringBoot.Clinica.Service.Mapper;
 import com.SpringBoot.Clinica.Entity.UserEntity;
 import com.SpringBoot.Clinica.model.UserDTO;
 import com.SpringBoot.Clinica.model.UserRequestDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserMapper {
 
-    public UserEntity map(UserDTO user);
-    public UserDTO map(UserRequestDTO user);
-    public UserDTO map(UserEntity user);
+    public UserEntity map(UserDTO userDTO) throws Exception;
+    public UserEntity map(UserRequestDTO userDTO) throws Exception;
+    public UserDTO map(UserEntity user) throws Exception;
 
 
 }
